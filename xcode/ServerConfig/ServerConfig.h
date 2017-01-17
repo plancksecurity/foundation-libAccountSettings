df7745877a7594d9e9f927ccda0202ed511df199
Dirk Zimmermann <dirk@pep-project.org>
2017-01-17 10:34:11 +0100

    xcode project

---
+++ xcode/ServerConfig/ServerConfig.h
@@ -0,0 +1,19 @@
+//
+//  ServerConfig.h
+//  ServerConfig
+//
+//  Created by Dirk Zimmermann on 17/01/2017.
+//  Copyright © 2017 pEp Security S.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ServerConfig.
+FOUNDATION_EXPORT double ServerConfigVersionNumber;
+
+//! Project version string for ServerConfig.
+FOUNDATION_EXPORT const unsigned char ServerConfigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ServerConfig/PublicHeader.h>
+
+
