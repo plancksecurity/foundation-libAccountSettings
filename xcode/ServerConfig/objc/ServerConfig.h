0802425d5b3ee34fe1ac54b1fc75245eafa70766
Dirk Zimmermann <dirk@pep-project.org>
2017-01-18 12:08:35 +0100

    IOS-410 changes for xcode

--- xcode/ServerConfig/objc/ServerConfig.h
+++ xcode/ServerConfig/objc/ServerConfig.h
@@ -8,6 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+//! Project version number for ServerConfig.
+FOUNDATION_EXPORT double ServerConfigVersionNumber;
+
+//! Project version string for ServerConfig.
+FOUNDATION_EXPORT const unsigned char ServerConfigVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ServerConfig/PublicHeader.h>
+
 #import "server_config.h"
 
 @interface ConnConfig : NSObject
