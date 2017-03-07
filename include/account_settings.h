a1840d0610a5029a3931d0633d92044099c1dd96
Roker <roker@pep-project.org>
2017-03-07 12:16:05 +0100

    add protocol enum

--- include/account_settings.h
+++ include/account_settings.h
@@ -19,6 +19,12 @@
 
 struct AccountSettings;
 
+AccountSettings* create_account_settings(void);
+
+void free_account_settings(AccountSettings* account_settings);
+
+
+
 typedef enum _AS_STATUS
 {
 	AS_OK = 0,
@@ -30,13 +36,33 @@ typedef enum _AS_STATUS
 //! The flags control the methods how get_account_settings() shall retrieve the account settings
 typedef enum _AS_FLAGS
 {
-	AS_FLAG_USE_ISP_DB     = 0x0010, ///<  use local ISP database
-	AS_FLAG_USE_HEURISTICS = 0x0020, ///<  use heuristics from account's domain name
-	AS_FLAG_USE_ANY_LOCAL  = 0x00FF, ///<  use all local (without communication to 3rd parties) methods in ascending order
-	AS_FLAG_USE_AUTOCONFIG = 0x0100, ///<  fetch account config from "autodiscover/autoconfig" servers
-	AS_FLAG_USE_ANY        = 0xFFFF  ///<  try first local, than network methods
+	AS_FLAG_USE_LOCAL_ISP_DB   = 0x0010, ///<  use local ISP database
+	AS_FLAG_USE_HEURISTICS     = 0x0020, ///<  use heuristics from account's domain name
+	AS_FLAG_USE_ANY_LOCAL      = 0x00FF, ///<  use all local (without communication to 3rd parties) methods in ascending order
+	AS_FLAG_USE_DNS_AUTOCONFIG = 0x0100, ///<  fetch account config from "autodiscover/autoconfig" servers
+	AS_FLAG_USE_ONLINE_ISP_DB  = 0x0800, ///<  ask https://live.mozillamessaging.com/autoconfig/v1.1/$DOMAINNAME
+	AS_FLAG_USE_ANY            = 0xFFFF  ///<  try first local, than network methods
 } AS_FLAGS;
 
+
+//! Known protocols
+typedef enum _AS_PROTOCOL
+{
+	// Incoming
+	AS_PROTO_POP3          = 0x1100;
+	AS_PROTO_POP3S         = 0x1110;
+	AS_PROTO_POP3_STARTTLS = 0x1120;
+	
+	AS_PROTO_IMAP          = 0x1200;
+	AS_PROTO_IMAPS         = 0x1210;
+	AS_PROTO_IMAP_STARTTLS = 0x1220;
+	
+	// Outgoing
+	AS_PROTO_SMTP          = 0x2100;
+	AS_PROTO_SMTPS         = 0x2110;
+	AS_PROTO_SMTP_STARTTLS = 0x2120;
+	
+} AS_PROTOCOL;
 
 /** get account settings - the main API function.
  *
@@ -61,6 +87,15 @@ typedef struct _as_provider
  *  @return a pointer to a static array of known providers. The array is terminated by a {NULL,NULL} entry.
  */
 const as_provider* get_known_providers();
+
+
+/** get provider
+ *
+ * @param  accountSettings  guess what
+ * @return the provider associated with the accountSettings. The pointer points to internal r/o data in accountSettings, do not delete it!
+ */
+const as_provider* get_provider(const AccountSettings* accountSettings);
+
 
 
 #ifdef __cplusplus
