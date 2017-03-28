56bff70383d9a90038376e20fed7e1c9cb89f881
Roker <roker@pep-project.org>
2017-03-28 14:15:57 +0200

    erste Gehversuche mit LibTinyXML2 :-)

--- src/account_settings_internal.hh
+++ src/account_settings_internal.hh
@@ -1,9 +1,28 @@
 // internal header of libAccountSettings.
 // DO NOT USE THAT IN APPLICATION CODE!
 
+#ifndef ACCOUNT_SETTINGS_INTERNAL_HH
+#define ACCOUNT_SETTINGS_INTERNAL_HH
+
 #include "account_settings.h"
+#include <string>
+#include <vector>
+
+struct Server
+{
+	std::string name;
+	int         port = -1;
+	AS_PROTOCOL protocol = AS_PROTOCOL(-1);
+	AS_USERNAME username = AS_USERNAME(-1);
+};
 
 struct AccountSettings
 {
-	
+	std::string id;
+	std::string displayName;
+	std::vector<std::string> domains;
+	Server incoming;
+	Server outgoing;
 };
+
+#endif // ACCOUNT_SETTINGS_INTERNAL_HH
--- src/xml2cxx.cc
+++ src/xml2cxx.cc
@@ -3,14 +3,82 @@
 
 #include "account_settings_internal.hh"
 #include "tinyxml2.hh"
-#include <cstdio>
+#include <iostream>
+
+std::vector<AccountSettings> vas;
+
+namespace tx = tinyxml2;
+
+
+std::string assignMember(const tx::XMLElement* elem, const char* memberName, const char* message)
+{
+	const tx::XMLElement* member = elem->FirstChildElement(memberName);
+	if(member)
+	{
+		return member->GetText();
+	}else{
+		if(message)
+		{
+			std::cerr << "PROBLEM: " << message;
+		}
+	}
+	return std::string();
+}
+
+
+std::vector<std::string> assignAllMembers(const tx::XMLElement* elem, const char* memberName)
+{
+	std::vector<std::string> v;
+	for(const tx::XMLElement* e = elem->FirstChildElement(memberName); e!=nullptr; e=e->NextSiblingElement(memberName) )
+	{
+		v.emplace_back( e->GetText() );
+	}
+	return v;
+}
+
 
 int main(int argc, char** argv)
 {
+	std::cerr << "Reading " << (argc-1) << "file" << ((argc-1)==1 ? "" : "s") << ":\n";
 	for(int a=1; a<argc; ++a)
 	{
-		tinyxml2::XMLDocument doc;
+		std::cerr << "\t #" << a << " : \"" << argv[a] << "\":  ";
+		tx::XMLDocument doc;
 		doc.LoadFile(argv[a]);
+		const tx::XMLElement* clientConfig = doc.FirstChildElement("clientConfig");
+		if(clientConfig==nullptr)
+		{
+			std::cerr << "ERROR: not a valid clientConfig!";
+			continue;
+		}
+		const tx::XMLElement* emailProvider = clientConfig->FirstChildElement("emailProvider");
+		if(emailProvider==nullptr)
+		{
+			std::cerr << "ERROR: not a valid clientConfig! <emailProvider> not found!";
+			continue;
+		}
 		
+		AccountSettings as;
+		
+		const char* id = emailProvider->Attribute("id");
+		if(id==nullptr)
+		{
+			std::cerr << "PROBLEM: <emailProvider> has no attribute id. :-o";
+		}else{
+			as.id = id;
+			std::cerr << "id=" << as.id;
+		}
+		as.displayName = assignMember(emailProvider, "displayName", "No displayName member");
+		as.domains = assignAllMembers(emailProvider, "domain");
+		std::cerr << " dN=\"" << as.displayName << "\", " << as.domains.size() << " domains";
+		if(as.domains.size()>0 && as.domains.size()<11)
+		{
+			std::cerr << ":";
+			for(const auto& d : as.domains)
+			{
+				std::cerr << " " << d;
+			}
+		}
+		std::cerr << ".  Done.\n";
 	}
 }
