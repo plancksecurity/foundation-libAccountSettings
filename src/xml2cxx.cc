8297c523824bf995162956b4a54115903b03566a
Roker <roker@pep-project.org>
2017-03-29 14:05:57 +0200

    implement parsing of the whole clientConfig/emailProvider stuff.

--- src/xml2cxx.cc
+++ src/xml2cxx.cc
@@ -4,13 +4,48 @@
 #include "account_settings_internal.hh"
 #include "tinyxml2.hh"
 #include <iostream>
+#include <map>
 
 std::vector<AccountSettings> vas;
 
 namespace tx = tinyxml2;
 
+const std::map<std::string, unsigned> mproto =
+	{
+		// protocols
+		{"pop3", 0x11000},
+		{"imap", 0x12000},
+		{"smtp", 0x21000},
+	};
 
-std::string assignMember(const tx::XMLElement* elem, const char* memberName, const char* message)
+const std::map<std::string, unsigned> mauth =
+	{
+		// authentication
+		{"none"              , 0x00001},
+		{"client-IP-address" , 0x00005},
+		{"plain"             , 0x00010}, // is it the same as "password-cleartext"?
+		{"password-cleartext", 0x00011},
+		{"password-encrypted", 0x00020},
+		{"OAuth2"            , 0x00028},
+	};
+
+const std::map<std::string, unsigned> msocket =
+	{
+		// socketType
+		{"plain"   , 0x00100},
+		{"STARTTLS", 0x00200},
+		{"SSL"     , 0x00300},
+	};
+
+const std::map<std::string, AS_USERNAME> musername =
+	{
+		{"%EMAILADDRESS%",  AS_USERNAME_EMAIL_ADDRESS},
+		{"%EMAILLOCALPART%", AS_USERNAME_EMAIL_LOCALPART},
+		{"%EMAILLOCALPART%.%EMAILDOMAIN%", AS_USERNAME_EMAIL_LOCALPART_DOMAIN },
+	};
+
+
+std::string assignMember(const tx::XMLElement* elem, const char* memberName, const char* message = nullptr)
 {
 	const tx::XMLElement* member = elem->FirstChildElement(memberName);
 	if(member)
@@ -19,7 +54,9 @@ std::string assignMember(const tx::XMLElement* elem, const char* memberName, con
 	}else{
 		if(message)
 		{
-			std::cerr << "PROBLEM: " << message;
+			std::cerr << message;
+		}else{
+			std::cerr << "PROBLEM: No \"" << memberName << "\"! ";
 		}
 	}
 	return std::string();
@@ -41,7 +78,22 @@ std::vector<Server> assignServers(const tx::XMLElement* elem, const char* member
 	std::vector<Server> v;
 	for(const tx::XMLElement* e = elem->FirstChildElement(memberName); e!=nullptr; e=e->NextSiblingElement(memberName) )
 	{
-		// TODO!
+		Server server;
+		unsigned protocol = 0;
+		const std::string type = e->Attribute("type");
+		protocol |= mproto.at(type);
+		
+		server.name = assignMember(e, "hostname");
+		server.port = std::stoi(assignMember(e, "port"));
+		const std::string auth = assignMember(e, "authentication");
+		const std::string socketType = assignMember(e, "socketType");
+		protocol |= mauth.at(auth);
+		protocol |= msocket.at(socketType);
+		server.protocol = AS_PROTOCOL(protocol);
+		
+		const std::string username = assignMember(e, "username");
+		server.username = musername.at(username);
+		v.push_back(std::move(server));
 	}
 	return v;
 }
@@ -50,7 +102,7 @@ std::vector<Server> assignServers(const tx::XMLElement* elem, const char* member
 bool serverPreference(const Server& a, const Server& b)
 {
 	// TODO: define a proper preference
-	return a.protocol < b.protocol;
+	return a.protocol > b.protocol;
 }
 
 
@@ -87,16 +139,6 @@ int main(int argc, char** argv)
 		}
 		as.displayName = assignMember(emailProvider, "displayName", "No displayName member");
 		as.domains = assignAllMembers(emailProvider, "domain");
-		std::cerr << " dN=\"" << as.displayName << "\", " << as.domains.size() << " domains";
-		if(as.domains.size()>0 && as.domains.size()<11)
-		{
-			std::cerr << ":";
-			for(const auto& d : as.domains)
-			{
-				std::cerr << " " << d;
-			}
-		}
-		
 		std::vector<Server> incomingServers = assignServers(emailProvider, "incomingServer");
 		std::sort(incomingServers.begin(), incomingServers.end(), &serverPreference);
 		if(incomingServers.size()>0)
@@ -111,7 +153,8 @@ int main(int argc, char** argv)
 			as.outgoing = outgoingServers.front();
 		}
 		
-		
+		std::cerr << '\n' << as;
 		std::cerr << ".  Done.\n";
+		vas.push_back(std::move(as));
 	}
 }
