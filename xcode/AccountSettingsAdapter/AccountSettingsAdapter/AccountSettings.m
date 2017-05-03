903f7795a8efc1a796ddc31c1706444739c395c6
Xavier Algarra <xavier@pep-project.org>
2017-05-03 16:53:58 +0200

    IOS-53 AcountSettings constructor

--- xcode/AccountSettingsAdapter/AccountSettingsAdapter/AccountSettings.m
+++ xcode/AccountSettingsAdapter/AccountSettingsAdapter/AccountSettings.m
@@ -7,11 +7,30 @@
 //
 
 #import "AccountSettings.h"
+#import "account_settings.h"
 
 @interface ASAccountSettings ()
-//@property (nonatomic, nonnull) AccountSettingsAS;
+
+@property (nonatomic, nonnull) struct AccountSettings *as;
+
+- (instancetype) initWithAccountSettings:(struct AccountSettings * _Nonnull)internalAccountSettings;
+
 @end
 
 @implementation ASAccountSettings
+
++ (ASAccountSettings * _Nullable)aSAccountSettingsWithAccountName:(NSString * _Nonnull)accountName
+                                                         provider:(NSString * _Nullable)provider
+                                                            flags:(AS_FLAGS)flags
+                                                      credentials:(void * _Nullable)credentials
+{
+    struct AccountSettings *as = get_account_settings([[accountName precomposedStringWithCanonicalMapping] UTF8String], [[provider precomposedStringWithCanonicalMapping] UTF8String], flags, credentials);
+
+    ASAccountSettings *acountsettings = [[ASAccountSettings alloc] initWithAccountSettings:(as)];
+
+    return acountsettings;
+}
+
+
 
 @end
